46c647b3327a4be41d7be7707a5757bab84e440e
Saloni Verma <saloni.verma@centricconsulting.com>
2024-01-18 19:01:13 +0530

    project added

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React  from "react";
+
+
+const Home = () => {
+    return (
+        <div ><h1>Welcome to MS EXIMP (Deepak Auto) Hypothetication Tool</h1>
+        <hr/>
+        
+  <div >
+  {/* <img className="profile-photo" src={require("C:\React\tswi-frontend\src\ImageAuto.gif")} alt={"Carlie Anglemire"}/> */}
+  
+  </div>
+    </div>);
+}
+
+export default Home;
